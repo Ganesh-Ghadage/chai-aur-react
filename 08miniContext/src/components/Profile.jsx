23865a6b60859246a35c4ff47cb81d6678737aba
Ganesh Ghadage <ganeshvg22@gmail.com>
2024-08-09 13:11:55 +0530

    Project 08 : Learning Context

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React, {useContext} from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+
+    const {user} = useContext(UserContext)
+
+    if (!user) return <div>Please Login</div>
+
+    return (
+        <div>
+            <h2>Profile</h2>
+            <p>Username: {user.username}</p>
+            <p>Password: {user.password}</p>
+        </div>
+    )
+}
+
+export default Profile
