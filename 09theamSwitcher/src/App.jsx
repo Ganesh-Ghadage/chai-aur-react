d70b8e5d62d6b17290817e2c24814b2dde4a5394
Ganesh Ghadage <ganeshvg22@gmail.com>
2024-08-11 13:05:36 +0530

    Project 09: Theme Switcher

--- 09theamSwitcher/src/App.jsx
+++ 09theamSwitcher/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <ThemeProvider value={{themeMode, changeMode}}>
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex flex-wrap min-h-screen items-center bg-gray-800 border-gray-700 dark:bg-white text-white">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
               <ThemeBtn/>
